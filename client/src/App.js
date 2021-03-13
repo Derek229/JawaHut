abbd6e6829340bb4d860559c5a47bab9909f4d80
Derek cluff <cluffderek@yahoo.com>
2021-03-13 12:11:43 -0700

    bav bar and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Fragment, } from 'react';
 import { Route, Switch, } from 'react-router-dom';
 import Home from './Home';
 import Planets from './Planets';
 import Droids from './Droids';
 import NavBar from './Components/NavBar'
 import {Container} from "semantic-ui-react"
-
 function App() {
   return (
+   
   <>
-    <NavBar/>
-    <Container>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path='/planets' component={Planets} />
-          <Route exact path='/planets/:id' component={Droids}/>
-        </Switch>
-        </Container>
+  <NavBar/>
+  <Container>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path='/planets' component={Planets} />
+        <Route exact path='/planets/:id' component={Droids}/>
+      </Switch>
+      </Container>
   </>
+  
 );
 
 
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import {Link, link} from 'react-router-dom'
+import {Button, button} from "semantic-ui-react"
 
-function Home() {
-  return (
-    <div>
-      
-    </div>
+export default () => {
+  return(
+    <>
+    <h1>Home</h1>
+    <Link to='/planets'><Button>Planets</Button></Link>
+    <Link to= '/droids'><Button>Droids</Button></Link>
+    <Link to='/about'><Button>About</Button></Link>
+    </>
   )
 }
 

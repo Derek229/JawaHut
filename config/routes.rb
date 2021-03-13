d6a765f665faa274be395d577b8db74700bb1140
Derek cluff <cluffderek@yahoo.com>
2021-03-13 10:31:50 -0700

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-namespace :api 
+namespace :api do
 resources :planets do
   resources :droids
+  end
+
+
+  resources :jobs 
+  end
+
 end
-
-
-# resources :jobs

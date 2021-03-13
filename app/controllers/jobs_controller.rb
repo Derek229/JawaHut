340b17e5482673d7d3df56adefbb60015132f981
Callice Austin <callice.a@gmail.com>
2021-03-13 14:43:18 -0700

    jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,39 @@
 class JobsController < ApplicationController
+
   def index
+    render json: Job.all
   end
 
   def show
+    job = Job.find(params[:id])
+    render json: {job: job}
   end
 
-  def new
+  def create
+    job = Job.new(job_params)
+    if (job.save)
+      render json: { errors: job.errors }, status: 422
+    end
   end
 
-  def edit
+  def update
+    job = Job.find(params[:id])
+    if job.update(job_params)
+      render json: job
+    else
+      render json: { errors: job.errors }, status: 422
+    end
   end
+
+  def destroy
+    job = Job.find(params[:id]).destroy
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:name, :company, :hourly_rate)
+  end
+
+
 end
